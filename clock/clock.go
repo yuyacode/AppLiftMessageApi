15784996cecdcf321c882abe2729fe12a51de5ca
長塚裕也 <ny410144@gmail.com>
2024-11-28 10:45:58 +0900

    現在時刻と固定時刻を扱うclockパッケージ用意

---
+++ clock/clock.go
@@ -0,0 +1,21 @@
+package clock
+
+import (
+	"time"
+)
+
+type Clocker interface {
+	Now() time.Time
+}
+
+type RealClocker struct{}
+
+func (r RealClocker) Now() time.Time {
+	return time.Now()
+}
+
+type FixedClocker struct{}
+
+func (fc FixedClocker) Now() time.Time {
+	return time.Date(2024, 7, 20, 12, 33, 56, 0, time.UTC)
+}
