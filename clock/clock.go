a59c8bde48572a293099ba5ab8e6861eb22d7af5
長塚裕也 <ny410144@gmail.com>
2024-12-07 15:15:51 +0900

    UTC時刻ではなくJST時刻として返す

--- clock/clock.go
+++ clock/clock.go
@@ -17,5 +17,6 @@ func (r RealClocker) Now() time.Time {
 type FixedClocker struct{}
 
 func (fc FixedClocker) Now() time.Time {
-	return time.Date(2024, 7, 20, 12, 33, 56, 0, time.UTC)
+	jst := time.FixedZone("JST", 9*60*60)
+	return time.Date(2024, 7, 20, 12, 33, 56, 0, jst)
 }
