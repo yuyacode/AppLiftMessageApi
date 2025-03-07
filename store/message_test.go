aa2f6dd751672366cbcee7e1be6a09784ea8a98d
長塚裕也 <ny410144@gmail.com>
2025-03-07 20:21:34 +0900

    store層の様々なテストメソッドから利用される型や関数を専用のファイルに切り出す

--- store/message_test.go
+++ store/message_test.go
@@ -8,36 +8,12 @@ import (
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/yuyacode/AppLiftMessageApi/clock"
 	"github.com/yuyacode/AppLiftMessageApi/entity"
 )
-
-type mockError struct {
-	msg string
-}
-
-func (m *mockError) Error() string {
-	return m.msg
-}
-
-func assertAnError() error {
-	return &mockError{"some db error"}
-}
-
-func newMockDB(t *testing.T) (*sqlx.DB, sqlmock.Sqlmock) {
-	t.Helper()
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		db.Close()
-	})
-	sqlxDB := sqlx.NewDb(db, "mysql")
-	return sqlxDB, mock
-}
 
 func TestMessageRepository_GetThreadCompanyOwner(t *testing.T) {
 	sqlxDB, mock := newMockDB(t)
